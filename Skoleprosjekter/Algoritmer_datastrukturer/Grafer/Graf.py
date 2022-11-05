b265184e5fe8f2354564cda67c1216e152621814
Guro Tronsmoen <gtronsm@hotmail.com>
2022-11-05 16:47:02 +0100

    Startet med dijkstra-algoritmen

--- Skoleprosjekter/Algoritmer_datastrukturer/Grafer/Graf.py
+++ Skoleprosjekter/Algoritmer_datastrukturer/Grafer/Graf.py
@@ -13,7 +13,9 @@
 # Gir en mulig «gjennomføringsplan»
 # Dersom det ikke gaar aa topologisk sortere nodene, saa kan vi konkludere med at grafen inneholder en sykel!
 
-import sys
+#Dijkstra:
+
+from collections import defaultdict
 
 
 class Graf:
@@ -81,7 +83,7 @@ class Graf:
                     koe.append(kant)
 
 
-class RettetGraf:  
+class Rettet_graf:  
     def __init__(self):
         self.noder = {}
         self.fil = "inputRettet.txt"
@@ -91,7 +93,7 @@ class RettetGraf:
             self.biter = linje.split(":")
             kanter = self.biter[1].strip('\n').split(",")
             
-            if self.biter[0] not in self.noder.keys():
+            if self.biter[0] not in self.noder.keys() and not self.biter[0] == '':
                 node = Node(self.biter[0])
             else:
                 node = self.noder.get(self.biter[0])
@@ -99,7 +101,7 @@ class RettetGraf:
             #Legger inn som navn:node i ordboken noder
             self.noder[self.biter[0]] = node
             
-            if self.biter[1] != '':
+            if not kanter[0] == '':
                 for kant in kanter:
                     if kant not in self.noder.keys():
                         kantnode = Node(kant)
@@ -112,17 +114,12 @@ class RettetGraf:
                     node.utgrad += 1
                     kantnode.inngrad += 1
         
-        self.TopSortering()
-        
-        
     #Topologisk sortering
-    def TopSortering(self):
+    def top_sortering(self):
         stack = []
         output = []
     
-        for node in self.noder:
-            print(self.noder[node].navn + " inngrad", self.noder[node].inngrad)
-          
+        for node in self.noder:          
             # Ser etter noder med inngrad 0 - disse kan vi starte med
             if self.noder[node].inngrad == 0:
                 stack.append(self.noder[node])
@@ -130,12 +127,10 @@ class RettetGraf:
         # Gaar i lokke over valgene som apner seg
         while len(stack) != 0:
             curNode = stack.pop()
-            print("Cur node", curNode.navn)
             output.append(curNode.navn)
             
-            if (curNode.kanter.size() != 0):
+            if (curNode.kanter != 0):
                 for kant in curNode.kanter:
-                    print("kant", kant.navn)
                     curNode.kanter.remove(kant)
                     kant.inngrad -= 1
                     curNode.utgrad -= 1
@@ -147,34 +142,109 @@ class RettetGraf:
         if len(output) > len(self.noder):
             print("ERROR: Graf inneholder en sykel og kan ikke sorteres topologisk")
 
+
+class VektetGraf:
+    def __init__(self):
+        self.noder = {}
+        self.fil = "inputVektet.txt"
+        self.filen = open(self.fil, "r")
+        
+        for linje in self.filen:
+            self.biter = linje.strip('\n').split()
+            self.vekt = self.biter[1]
+            self.nodene = self.biter[0].split("-")
+            
+            if self.nodene[0] not in self.noder.keys():
+                print(self.nodene[0], "IKKE FRA FOER")
+                node1 = Node(self.nodene[0])
+                self.noder[self.nodene[0]] = node1
+                
+                if self.nodene[1] not in self.noder.keys():
+                    print(self.nodene[1], "IKKE FRA FOER")
+                    node2 = Node(self.nodene[1])
+                    self.noder[self.nodene[1]] = node2
+
+                    node1.vektKanter[node2] = float(self.vekt)
+                    node2.vektKanter[node1] = float(self.vekt)
+                
+                else:
+                    self.noder.get(self.nodene[0]).vektKanter[self.noder.get(self.nodene[1])] = float(self.vekt)
+                    self.noder.get(self.nodene[1]).vektKanter[self.noder.get(self.nodene[0])] = float(self.vekt)
+                    
+            else:
+                if self.nodene[1] not in self.noder.keys():
+                    print(self.nodene[1], "IKKE FRA FOER")
+                    node2 = Node(self.nodene[1])
+                    self.noder[self.nodene[1]] = node2
+                    
+                    self.noder.get(self.nodene[0]).vektKanter[node2] = float(self.vekt)
+                    node2.vektKanter[self.noder.get(self.nodene[0])] = float(self.vekt)
+                    
+                else:
+                    self.noder.get(self.nodene[0]).vektKanter[self.noder.get(self.nodene[1])] = float(self.vekt)
+                    self.noder.get(self.nodene[1]).vektKanter[self.noder.get(self.nodene[0])] = float(self.vekt)
+
+        for node in self.noder.keys():
+            print(self.noder.get(node).navn , "sine kanter: ")
+            for kant in self.noder.get(node).vektKanter:
+                print(kant.navn)
+            
+            print(self.noder.get(node).vektKanter)
+
+    def Dijkstra(self):
+        #Tar egentlig inn en startnode, men her setter vi den bare
+        startnode = self.noder.get(0)
+        
+        visited = set()
+        dist = defaultdict(lambda: float('inf'))
+        
+        
+        pass
+            
     
+
 class Node:
     def __init__(self, navn):
         self.navn = navn
         self.inngrad = 0
         self.utgrad = 0
         self.kanter = []
+        self.vektKanter = {}
      
-
-
-print("Onsker du en: \n")
-print("(Tast r) Rettet graf")
-print("(Tast u) Urettet graf")
-
-x = input("\nDitt svar: ")
-
 #HOVEDPROGRAM
-if (x.lower() == "r"):
-    graf = RettetGraf()
+x = "true"
+
+while x == "true":
+# while (not x == "r" or not x == "u"):
+    print("_____________________")
+    print("     VALGMENY \n")
+    print("(Tast r) Rettet graf")
+    print("(Tast u) Urettet graf")
+    print("(Tast v) Vektet graf")
+    print("(Tast n) Negativt vektet graf")
+    print("(Tast a) Avslutt")
+    x = input("\nDitt svar: ")
+    if (x.lower() == "r"):
+        graf = Rettet_graf()
+        print("\nTopologisk: ")
+        graf.top_sortering()
+        
+    elif (x.lower() == "u"):
+        graf = Graf()
+        print("\nDybdeFoerst: ")
+        graf.DFSoekHele()
+        print("\nBreddeFoerst: ")
+        graf.BFSoekHele()
     
-elif (x.lower() == "u"):
-    graf = Graf()
-    print("DybdeFoerst: ")
-    graf.DFSoekHele()
-    print("\nBreddeFoerst: ")
-    graf.BFSoekHele()
+    elif (x.lower() == "v"):
+        graf = VektetGraf()
     
-else:
-    print("\nUgyldig svar. Programmet avsluttes.")
-    exit()
-    
+    elif (x.lower() == "n"):
+        pass
+        
+    elif (x.lower() == "a"):
+        exit()
+        
+    else:
+        print("\nUgyldig svar.\n")
+        x = "false"
